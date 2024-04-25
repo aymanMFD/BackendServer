aeacea0cfa5650eeff90d36f27b24c5a6d6532a3
aymanMFD <otherayman3@gmail.com>
2024-04-25 10:02:00 +0400

    New updates

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import { Client } from 'basic-ftp';
 
-async function Data() {
+const address = 'eu-central-1.sftpcloud.io';
+const user = '9bf46f771b9941698c88544264493a31';
+const password = 'p0L5RScxWB7aKpcHOHrnfDyAq3vbaqJE';
+
+const Data = async (addr, user, password) => {
     const client = new Client();
     var data;
     client.ftp.verbose = false;
     try {
         await client.access({
-            host: 'eu-central-1.sftpcloud.io',
-            user: '1375dcb65a7d4f15816b7f7e2f524d5f',
-            password: 'uPK4bVnGQ46m0NnNhHrNBXytKNVbyLoZ',
+            host: addr,
+            user: user,
+            password: password,
             secure: false
         })
         data = (await client.list()).length;
@@ -20,15 +24,15 @@ async function Data() {
     }    
 }
 
-const getData = async () => {
-    const result = await Data();
+const getData = async (addr, user, password) => {
+    const result = await Data(addr, user, password);
     return result;
 }
 
 const app = express();
 
-app.get('/api/data', function(req, res) {
-    getData().then(count => {
+app.get('/sendData', function(req, res) {
+    getData(address, user, password).then(count => {
         const data = {
             NumberOfFiles: count
         };
@@ -37,4 +41,16 @@ app.get('/api/data', function(req, res) {
    
 });
 
-app.listen(3000, () => {console.log("Server is listening on port 3000")});
+app.use(express.json()); 
+app.post('/getData', (req, res)=>{ 
+    const {name} = req.body; 
+      
+    res.send(`Welcome ${name}`); 
+}) 
+app.listen(3000, (err) => {
+    if (!err) {
+    console.log("[*]Server is listening on port 3000")
+    } else {
+        console.log("An error has occured!")
+    }
+});
