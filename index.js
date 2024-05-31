a28be977efa4556ba5900a18413c2f857119f712
aymanMFD <otherayman3@gmail.com>
2024-05-31 11:39:05 +0400

    Added comments for better understanding

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+///////////////////////////////////////////////////////////////////////////////////
+// URL to API: https://backendserver-b2rl.onrender.com
+///////////////////////////////////////////////////////////////////////////////////
+
 import express from 'express';
 import { Client } from 'basic-ftp';
 
@@ -5,6 +9,8 @@ const Data = async (addr, user, password, port, folders) => {
     const client = new Client();
     var data;
     client.ftp.verbose = false;
+
+    // establishing connection
     try {
         await client.access({
             host: addr,
@@ -16,12 +22,21 @@ const Data = async (addr, user, password, port, folders) => {
         data = (await client.list()).length;
         let folderCount = {};
         let fileCountInFolder = 0;
+
+        // Iterate through the list of folders that are passed through the URL 
         for (let i = 0; i < (folders.paths).length; i++) {
-            let processedFolderPath = folders.paths[i].replaceAll("@", "/");
+
+            // Subpaths are shown as '/' and when passed to a URL, it can create a clash as URL considers '/' as another path
+            // When passing the folders and subfolders through the URL, in the mobile app itself will convert the '/' to '@'
+            let processedFolderPath = folders.paths[i].replaceAll("@", "/"); // Converting the '@' back to '/' to process.
+
+            // 'cd' into the path and then checking how many files there are in the directory
             await client.cd(processedFolderPath);
             fileCountInFolder = (await client.list()).length;
             folderCount[folders.paths[i]] = fileCountInFolder;
-            await client.cd("/");
+
+            // returning back to the primary directory to then access another folder in the next iteration
+            await client.cd("/"); 
         }
         // The total number of files in the server
         folderCount["NumberOfFiles"] = data;
@@ -33,10 +48,13 @@ const Data = async (addr, user, password, port, folders) => {
     }    
 }
 
+
+// This function will only check if a folder exists or not in a server and return an adequate response
 const CheckFolder = async (addr, user, password, folderPath, port) => {
     const client = new Client();
     
     client.ftp.verbose = false;
+    // establishing connection 
     try {
         await client.access({
             host: addr,
@@ -45,7 +63,7 @@ const CheckFolder = async (addr, user, password, folderPath, port) => {
             secure: false,
             port: port
         })
-        const response = await client.cd(folderPath);
+        const response = await client.cd(folderPath); // checking if folder exists by 'cd' in the directory 
         
         return response.code
     } catch (err) {
@@ -53,10 +71,12 @@ const CheckFolder = async (addr, user, password, folderPath, port) => {
     }    
 }
 
+// This function will only check if a server exists or not and return an adequate response
 const NewConnection = async (addr, user, password, port) => {
     const client = new Client();
     client.ftp.verbose = false;
 
+    // establishing connection
     try {
         await client.access({
             host: addr,
@@ -74,6 +94,7 @@ const NewConnection = async (addr, user, password, port) => {
     }
 }
 
+// The functions below are only to retrieve the results from their respective functions
 const getFolderData = async (addr, user, password, folderPath, port) => {
     const result = await CheckFolder(addr, user, password, folderPath, port);
     return result;
@@ -93,6 +114,7 @@ const getNewConnection = async (addr, user, password, port) => {
 const app = express();
 const router = express.Router();
 
+// this route is used to get the number of files in the servers and respective paths
 router.get('/sendData/:address&:user&:password&:port&:folders', function(req, res) {
     
     getData(req.params.address, req.params.user, req.params.password, req.params.port, JSON.parse(req.params.folders)).then(folderCount => {
@@ -104,6 +126,7 @@ router.get('/sendData/:address&:user&:password&:port&:folders', function(req, re
    
 });
 
+// this route is only to check whether a path/folder exists
 router.get('/checkFolder/:address&:user&:password&:port&:folderPath', function(req, res) {
     const folders = req.params.folderPath.replaceAll("@", "/");
     getFolderData(req.params.address, req.params.user, req.params.password, folders, req.params.port).then(ans => {
@@ -115,6 +138,8 @@ router.get('/checkFolder/:address&:user&:password&:port&:folderPath', function(r
     })
 })
 
+
+// this route is only to check whether a ftp server exists
 router.get('/checkConnection/:address&:user&:password&:port', function(req, res) {
     getNewConnection(req.params.address, req.params.user, req.params.password, req.params.port).then(code => {
         console.log(`Connection code with ${req.params.address}: ${code}`);
